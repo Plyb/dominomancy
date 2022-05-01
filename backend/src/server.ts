68384ba07eaa8ff289b3516b09639a1c4839a376
Koby Lewis <kobyjlewis@gmail.com>
2022-04-30 20:55:42 -0600

    add basic playing card game state

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
 import StartServer from '@plyb/web-game-core-backend'
+import PlayingCardGameState from 'shared/src/gameState/PlayingCardGameState'
 
-StartServer([]);
+StartServer([], {}, (players) => new PlayingCardGameState(players));
---
+++ shared/src/gameState/PlayingCardGameState.ts
@@ -0,0 +1,14 @@
+import { BoardGameState, Player } from "@plyb/web-game-core-shared/src";
+import DrawPile from "@plyb/web-game-core-shared/src/model/gameState/pieces/DrawPile";
+import PlayingCard, { Suit } from "@plyb/web-game-core-shared/src/model/gameState/pieces/PlayingCardPiece";
+
+export default class PlayingCardGameState extends BoardGameState {
+    public constructor(players: Player[]) {
+        super(players, {x: 10, y: 10}, {x: 8, y: 8});
+
+        this.hub.placePiece(new DrawPile(PlayingCard.get52Cards()), 6, 4);
+        this.hub.placePiece(new DrawPile([], true), 2, 4);
+        this.hub.placePiece(new PlayingCard(0, Suit.Joker), 0, 0);
+        this.hub.placePiece(new PlayingCard(0, Suit.Joker), 1, 0);
+    }
+}
