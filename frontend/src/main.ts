2537af9a36599b97c65077bd2c87a408780bbb2a
Koby Lewis <kobyjlewis@gmail.com>
2022-04-30 19:58:02 -0600

    Migrate core vue components to core repository

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,37 +1,3 @@
-import Vue, { createApp, DirectiveBinding, VNode } from 'vue'
-import router from './router'
-import App from './App.vue'
+import { setUpVueApp } from "@plyb/web-game-core-frontend";
 
-type TaggedElement = Element & {
-  eventMap: {
-    [key: string]: (event: MouseEvent) => void
-  }
-}
-function createMouseOutsideDirective(eventType: 'mouseup' | 'click') {
-  return {
-    beforeMount: function (el: TaggedElement, binding: Vue.DirectiveBinding) {
-      function clickOutsideEvent(event: MouseEvent) {
-        // here I check that click was outside the el and his children
-        if (window.document.contains(event.target as Node)
-          &&!(el == event.target || el.contains(event.target as Node))) {
-          // and if it did, call method provided in attribute value
-          binding.value();
-        }
-      }
-      if (!el.eventMap) {
-        el.eventMap = {};
-      }
-      el.eventMap[eventType] = clickOutsideEvent;
-      document.addEventListener(eventType, clickOutsideEvent)
-    },
-    unmounted: function (el: any) {
-      document.removeEventListener(eventType, el.eventMap[eventType])
-    },
-}
-}
-
-const app = createApp(App);
-app.directive('mouseup-outside', createMouseOutsideDirective('mouseup'));
-app.directive('click-outside', createMouseOutsideDirective('click'));
-app.use(router);
-app.mount('#app');
+setUpVueApp();
